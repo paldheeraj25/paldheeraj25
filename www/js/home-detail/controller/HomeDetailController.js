8818b72a486667b2e4ef707ecad928c9feb6426a
Dheeraj <paldheeraj25@gmail.com>
2016-10-09 18:46:14 +0530

    tubelight added

--- www/js/home-detail/controller/HomeDetailController.js
+++ www/js/home-detail/controller/HomeDetailController.js
@@ -6,9 +6,13 @@ homeAppController.controller('HomeDetailController', function($http, $state, aut
     $state.go('login');
   }
   _instance.roomDetail = roomDetail;
+  console.log(_instance.roomDetail);
   _instance.toggleSwitch = function(deviceValue, deviceName){
+    console.log(deviceName);
+    console.log(deviceValue);
+    console.log('https://'+ _instance.roomDetail.domain + '/gpio2.php?cflon=OFF&callback=JSON_CALLBACK');
     if(deviceName === 'CFL' && deviceValue === true){
-      $http.jsonp('https://'+ _instance.roomDetail.domain + '/gpio2.php?off=OFF&callback=JSON_CALLBACK').success(function(data){
+      $http.jsonp('https://'+ _instance.roomDetail.domain + '/gpio2.php?cflon=OFF&callback=JSON_CALLBACK').success(function(data){
         console.log('success');
         console.log(data);
 	    })
@@ -16,12 +20,26 @@ homeAppController.controller('HomeDetailController', function($http, $state, aut
 	      console.log('error')
 	    });
     } else if(deviceName === 'CFL' && deviceValue === false){
-      $http.jsonp('https://'+ _instance.roomDetail.domain + '/gpio2.php?on=ON&callback=JSON_CALLBACK').success(function(data){
+      $http.jsonp('https://'+ _instance.roomDetail.domain + '/gpio2.php?cfloff=ON&callback=JSON_CALLBACK').success(function(data){
        console.log('success');
 	    })
 	    .error(function () {
 	      console.log('error')
 	    });
+    } else if(deviceName === 'Tube Light' && deviceValue === true){
+      $http.jsonp('https://'+ _instance.roomDetail.domain + '/gpio2.php?tubelighton=ON&callback=JSON_CALLBACK').success(function(data){
+       console.log('success');
+      })
+      .error(function () {
+        console.log('error')
+      });
+    } else if(deviceName === 'Tube Light' && deviceValue === false){
+      $http.jsonp('https://'+ _instance.roomDetail.domain + '/gpio2.php?tubelightoff=ON&callback=JSON_CALLBACK').success(function(data){
+       console.log('success');
+      })
+      .error(function () {
+        console.log('error')
+      });
     }
   };
 });
